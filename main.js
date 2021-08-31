3a4c76a5ba8d6f614a2bfb98a851b1dadcee35c6
Pia Torain <piatorain@yahoo.com>
2021-08-30 19:58:21 -0700

    Updated html.index button and main.js functions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    
-    const eval = document.getElementById("evaluation");
+    const form = document.getElementById("form");
+    // const eval = document.getElementById("evaluation");
     const button = document.getElementById("check-input");
     const str = document.getElementById("isogramText").value;
 
@@ -28,42 +28,24 @@ document.addEventListener("DOMContentLoaded", () => {
         isFalse.style.display = "none";
         isTrue.style.display = "none";
     };
-
+    
     button.addEventListener('click', function(e) {
         e.preventDefault();
         
     });
-    button.addEventListener('click', isIsogram);
+    button.addEventListener('click', evalString);
+    
     
    
     
-    function isIsogram(str) {
+    function evalString(str) {
+        
         str = document.getElementById("isogramText").value;
-       
-        // Determine the length of the string and assigned to variable lengthOfString
-        let lenthOfString = str.length;
-    
-        // Use split() method to split the string into an array of substrings and return a new array 
-            // (without changing the orignal array) assigned to variable strArray
-        var strArray = [];
-        var strArray = str.split('');
-    
-        // Use sort() method to sort the split string in alphabetical and ascending order by default 
-        // var strArray = strArray.sort();
-        // strArray = strArray.sort();
-
-       
     
         // Use a For Loop to check the string for special char, spaces and numbers as well as evaluate the array for duplicate substrings
             // Starting at index 0, as long as the index is less than the length of the string minus 1, add 1 to the index and continue the loop
-                // assign variables to regex expression for special characters and numbers
-                // assign letter variable to the lowercase substring at index i
-                // assign count variable to the value of 0.
-            // For Loop 
-                // Starting at index 0, as long as the index is less than the length of the string minus 1, add 1 to the index and continue the loop
-                // if letter is strictly equal to the lowrcase character at index n, increment the count by 1  
-                // if the count is equals 2 or is greater than 2, then the string is NOT a isogram 
-                    // Evaluation ends
+                // assign variables to regex expression for special characters and number
+    
             // IF any character in the string matches the regex pattern for special characters, numbers and has a space after the first character, the string will not be evaluated
                 // ELSE IF CONDITONS:
                     // STRING WILL NOT BE EVALUATED IF:
@@ -73,27 +55,13 @@ document.addEventListener("DOMContentLoaded", () => {
                         //  string has a space after the 1st character.
                         //  string has at least one number.
                         //  string has at least one special character.
-        // Otherwise, the string contains characters that do not repeat and IS an isogram.
-    
-    
-        for (let i = 0; i < lenthOfString - 1; i++){
+                    // If any condition is met, the users gets an alert and the form resets after 3sec
+                    // Otherwise, the users input meets the requirement and isIsogram() is called.
+
+        for (let i = 0; i < str.length - 1; i++){
             let specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
             let numbers = /\d/
-            var count = 0;
-            letter = strArray[i].toLowerCase();
-
-            for(var n = 0; n <= lenthOfString - 1; n ++){
-                if( letter === strArray[n].toLowerCase() ) {
-                    count += 1;
-                }
-            }
-            if(count == 2 || count > 2){
-                hideAll();
-                isFalse.style.display = "block";
-                isFalse.style.color = "#FF1919";
-                
-            }
-
+           
             if(specialChar.test(str) && numbers.test(str) && str.indexOf(' ') >= 1 ){
                 hideAll();
                 isAll.style.display = "block";
@@ -126,14 +94,64 @@ document.addEventListener("DOMContentLoaded", () => {
             }else if(numbers.test(str)) {
                 hideAll();
                 isNumbers.style.display = "block";
-            
-            }else {
-                hideAll();
-                isTrue.style.display = "block";
-                isTrue.style.color = "#00FF2A";
+
             }
+            else {
+                isIsogram();
+            }     
+            clearForm();
         };
-    }
+        function clearForm() {
+            setTimeout(function() {
+                form.reset();
+                hideAll();
+            }, 3000);
+
+        };
+        
+         
+        function isIsogram() {
+            var str = document.getElementById("isogramText").value;
+
+            // assign variable isogram to boolean true  -- funcion assumes isogram is true
+            var isogram = true;
+
+            // create empy arrays
+            var newLetters = [];
+            var letters = [];
+
+            // assign count variable to the value of 0.
+            var count = 0;
+            
+            // letters array is populated with substrings of the user's input
+            letters = str.toLowerCase().split('');
+        
+            // For Loop
+                //Check to see if the substrings in the array newLetters match any subtring in the array letters, index -1,
+                    // if there is NOT a match, add the substring to the newLetters array
+                        // the string IS an isogram
+                    // else, isogram is false,  the match occurred more than once (index > -1) 
+                        // the string is NOT an isogram
+            for (let i = 0; i < letters.length - 1; i++){
+                
+                if(newLetters.indexOf(letters[i]) == -1) {
+                    newLetters += letters[i];
+                    hideAll();
+                    isTrue.style.display = "block";
+                    isTrue.style.color = "#00FF2A";
+                }
+                else {
+                    isogram = false;
+                    hideAll();
+                    isFalse.style.display = "block";
+                    isFalse.style.color = "#FF1919";
+                }
+                clearForm();
+            };
+
+        }
+    }    
+    
     
 
 });
